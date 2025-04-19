b5008a291f0dc6c82dd96e2a02ab5d48df791616
Rudra Bhikadiya <irudradev@gmail.com>
2025-04-19 14:04:44 +0530

    inter

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className='font-[family-name:var(--font-geist-sans)]'>
+    <div className='font-[family-name:var(--font-inter-sans)]'>
       <h1 className='text-3xl'>Hello</h1>
       <p className='text-muted-foreground'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, fugit.</p>
     </div>
